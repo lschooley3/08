70af8f5dba377c8397b3ad4e3f2028c9f52290f6
Laura Schooley <laura.schooley3@gmail.com>
2024-11-04 21:03:08 -0600

    port fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,6 +19,8 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
 });
